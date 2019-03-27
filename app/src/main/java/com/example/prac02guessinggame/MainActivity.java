435cf694fe0efc33b7f92799d92b04527c0e63c7
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-03-27 22:52:54 +0800

    make the game work in the MainActivity alone

--- app/src/main/java/com/example/prac02guessinggame/MainActivity.java
+++ app/src/main/java/com/example/prac02guessinggame/MainActivity.java
@@ -3,14 +3,60 @@ package com.example.prac02guessinggame;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView statusText;
+    private EditText guessNum;
+
+    private int secretNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Random random = new Random();
+        secretNumber = 1 + random.nextInt(100);
+
+        statusText = (TextView) findViewById(R.id.statusText);
+        guessNum = (EditText) findViewById(R.id.guessNum);
+
+        statusText.setText("Guess the number:");
+        guessNum.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence string, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence string, int start, int before, int count) {
+
+                int value = Integer.parseInt(string.toString());
+
+                    if (secretNumber == value){
+                        statusText.setText("You won!");
+                }
+                    else if (secretNumber > value){
+                        statusText.setText("Try bigger");
+                    }
+                    else {
+                        statusText.setText("Try smaller");
+                    }
+                    
+            }
+
+            @Override
+            public void afterTextChanged(Editable string) {
+
+            }
+        });
     }
 
     public void handler(View view){
--- app/src/main/java/com/example/prac02guessinggame/SecondaryActivity.java
+++ app/src/main/java/com/example/prac02guessinggame/SecondaryActivity.java
@@ -7,10 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SecondaryActivity extends AppCompatActivity{
     private SeekBar seekbarMin;
     private SeekBar seekbarMax;
+    private TextView progressTextMin;
+    private TextView progressTextMax;
     private SharedPreferences preferences;
 
     @Override
@@ -20,11 +23,13 @@ public class SecondaryActivity extends AppCompatActivity{
 
         seekbarMin = (SeekBar) findViewById(R.id.simpleSeekBarMin);
         seekbarMax = (SeekBar) findViewById(R.id.simpleSeekBarMax);
+        progressTextMin = (TextView) findViewById(R.id.minNum);
+        progressTextMax = (TextView) findViewById(R.id.maxNum);
 
         preferences = getSharedPreferences("value", MODE_PRIVATE);
         Log.i("Secondary", "on create called");
-
     }
+
 
     @Override
     protected void onStart(){
@@ -35,7 +40,11 @@ public class SecondaryActivity extends AppCompatActivity{
         seekbarMin.setProgress(progressMin);
         int progressMax = preferences.getInt("seek bar max", 0);
         seekbarMax.setProgress(progressMax);
+
+        progressTextMin.setText("" + preferences.getInt("seek bar min", 0));
+        progressTextMax.setText("" + preferences.getInt("seek bar max", 0));
     }
+
 
     @Override
     protected void onStop(){
@@ -43,6 +52,7 @@ public class SecondaryActivity extends AppCompatActivity{
         Log.i("Secondary", "on stop called");
         preferences.edit().putInt("seek bar min", seekbarMin.getProgress()).apply();
         preferences.edit().putInt("seek bar max", seekbarMax.getProgress()).apply();
+
     }
 
     @Override
