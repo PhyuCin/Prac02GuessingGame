f8c229f7cbd8cfaa4d4321d7ad86ac3bb1365cc1
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-03-27 11:49:52 +0800

    connect the two activities with the buttons

--- app/src/main/java/com/example/prac02guessinggame/SecondaryActivity.java
+++ app/src/main/java/com/example/prac02guessinggame/SecondaryActivity.java
@@ -1,4 +1,18 @@
 package com.example.prac02guessinggame;
 
-public class SecondaryActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class SecondaryActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary);
+    }
+
+    public void handler(View view){
+        Intent intent = new Intent(this,MainActivity.class );
+        startActivity(intent);}
 }
